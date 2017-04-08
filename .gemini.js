ac55ccf66a610aece0a5fdd4877be80bd51734fc
TerAleS <terales@users.noreply.github.com>
2017-04-08 16:19:10 +0300

    test with gemini in a single command

--- .gemini.js
+++ .gemini.js
@@ -1,13 +1,12 @@
 module.exports = {
-    rootUrl: 'http://yandex.ru',
-    gridUrl: 'http://127.0.0.1:4444/wd/hub',
+  rootUrl: 'http://yandex.ru',
+  gridUrl: 'http://127.0.0.1:4444/wd/hub',
 
-    browsers: {
-        chrome: {
-            desiredCapabilities: {
-                browserName: 'chrome'
-            }
-        }
+  browsers: {
+    chrome: {
+      desiredCapabilities: {
+        browserName: 'chrome'
+      }
     }
+  }
 }
-
---
+++ gemini-runner.js
@@ -0,0 +1,26 @@
+const selenium = require('selenium-standalone')
+const Gemini = require('gemini/api')
+
+selenium.start(runGemini)
+
+function runGemini (err, seleniumInstance) {
+  const gemini = new Gemini()
+  const result = gemini.test('./gemini', { reporters: ['flat'] })  
+  result.then(reportGeminiResults.bind(null, seleniumInstance))
+  result.catch(logGeminiError)
+}
+
+function reportGeminiResults(seleniumInstance, report) {
+  seleniumInstance.kill()
+
+  const fails = report.errored + report.failed
+  if (fails > 0) {
+    process.exit(1) // Exit with error
+  }
+
+  process.exit() // Exit with success
+}
+
+function logGeminiError (error) {
+  console.log(error)
+}
