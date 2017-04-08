79c6bd8ba8e8b5f0dd57ccbea910da375cb65d05
TerAleS <terales@users.noreply.github.com>
2017-04-08 16:25:03 +0300

    rename tests folder to generic one

--- .gemini.js
+++ .gemini.js
@@ -2,6 +2,8 @@ module.exports = {
   rootUrl: 'http://yandex.ru',
   gridUrl: 'http://127.0.0.1:4444/wd/hub',
 
+  screenshotsDir: './test-e2e-screenshots/screens',
+
   browsers: {
     chrome: {
       desiredCapabilities: {
